11cb20fa2a61955abe58559e4732b6daefc0f56b
StylHard <Gauna.ignaciosh@hotmail.com>
2025-01-14 18:07:15 -0300

    aa

--- Proyecto/Proyecto_Final_1.py
+++ Proyecto/Proyecto_Final_1.py
@@ -11,6 +11,7 @@ from collections import Counter
 #holaaaaaaaaaaaaaaaaaaaaaamundoooooooooooooooooooooooooo
 #whatsapppppp
 #holadasdasdasdsda
+#ddasdasdasdasdasdasdsa
 
 #que onda
 def speak_async(engine, text):
